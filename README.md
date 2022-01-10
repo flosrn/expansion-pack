fe8351ba47bddc08bb7eb6ee03f230186245b3c9
Flosrn <flosrn.dev@gmail.com>
2022-01-10 22:43:37 +0100

    🎉 first commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# expansion-pack
+
+[nextjs-ts-starter](https://github.com/flosrn/nextjs-ts-starter) 
+expansion pack
+
+
+## Roadmap
+
+- [x] React Hook Form + Form Input Components
+- [x] Storybook
+- [x] Cypress
+- [ ] Authentication with Zustand
+- [X] Toast with React Query / SWR
+- [x] Dialog Manager with Zustand
+- [x] NProgress
+
+## React Hook Form
+
+[See the readme](https://github.com/flosrn/expansion-pack/blob/main/rhf/README.md)
+
+```bash
+curl -s https://raw.githubusercontent.com/flosrn/expansion-pack/main/rhf/trigger.sh | bash -s
+```
+
+## Storybook
+
+[See the readme](https://github.com/flosrn/expansion-pack/blob/main/storybook/README.md)
+
+```bash
+curl -s https://raw.githubusercontent.com/flosrn/expansion-pack/main/storybook/trigger.sh | bash -s
+```
+
+## Cypress
+
+[See the readme](https://github.com/flosrn/expansion-pack/blob/main/cypress/README.md)
+
+```bash
+curl -s https://raw.githubusercontent.com/flosrn/expansion-pack/main/cypress/trigger.sh | bash -s
+```
+
+## Toast with React Query / SWR
+
+[See the readme](https://github.com/flosrn/expansion-pack/blob/**main**/toast/README.md)
+
+### React Query
+
+```bash
+curl -s https://raw.githubusercontent.com/flosrn/expansion-pack/main/toast/trigger-rq.sh | bash -s
+```
+
+### SWR
+
+```bash
+curl -s https://raw.githubusercontent.com/flosrn/expansion-pack/main/toast/trigger-swr.sh | bash -s
+```
+
+## Dialog Manager with Zustand
+
+[See the readme](https://github.com/flosrn/expansion-pack/blob/main/dialog-zustand/README.md)
+
+```bash
+curl -s https://raw.githubusercontent.com/flosrn/expansion-pack/main/dialog-zustand/trigger.sh | bash -s
+```
+
+## NProgress
+
+[See the readme](https://github.com/flosrn/expansion-pack/blob/main/nprogress/README.md)
+
+```bash
+curl -s https://raw.githubusercontent.com/flosrn/expansion-pack/main/nprogress/trigger.sh | bash -s
+```
---
+++ rhf/trigger.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[1;32m'
+CYAN='\033[1;36m'
+NC='\033[0m'
+
+echo -e "${CYAN}========================="
+echo "React Hook Form Expansion"
+echo "========================="
+echo -e "${NC}"
+
+#region  //*=========== Install Packages ===========
+echo -e "${NC}"
+echo -e "${GREEN}[Step 1] Installing additional packages${NC}"
+echo ""
+echo -e "Packages: ${GREEN}react-hook-form react-datepicker react-dropzone react-image-lightbox"
+echo -e "${NC}"
+yarn add react-hook-form react-datepicker react-dropzone react-image-lightbox 
+echo -e ""
+echo -e "Dev Packages: ${GREEN}@types/react-datepicker"
+echo -e "${NC}"
+yarn add -D @types/react-datepicker 
+# endregion  //*======== Install Packages ===========
+
+#region  //*=========== Create Directories ===========
+mkdir -p src/components/forms
+mkdir -p src/pages/sandbox
+mkdir -p src/types
+#endregion  //*======== Create Directories ===========
+
+#region  //*=========== Downloading Files ===========
+echo ""
+echo -e "${GREEN}[Step 2] Downloading files${NC}"
+echo ""
+
+DIRNAME="rhf"
+
+files=(
+  "src/components/forms/DatePicker.tsx"
+  "src/components/forms/DropzoneInput.tsx"
+  "src/components/forms/FilePreview.tsx"
+  "src/components/forms/Input.tsx"
+  "src/components/forms/SelectInput.tsx"
+  "src/components/forms/TextArea.tsx"
+  "src/pages/sandbox/rhf.tsx"
+  "src/types/dropzone.ts"
+)
+for i in "${files[@]}"
+do
+  echo "Downloading... $i"
+  curl -LJs -o $i https://raw.githubusercontent.com/flosrn/expansion-pack/main/$DIRNAME/$i
+done
+
+# Append datepicker css reset
+echo "Appending additional css..."
+curl -s https://raw.githubusercontent.com/flosrn/expansion-pack/main/$DIRNAME/datepicker.css >> src/styles/globals.css
+#endregion  //*======== Downloading Files ===========
+
+
+echo ""
+echo -e "${CYAN}============================================"
+echo "🔋 React Hook Form Expansion Completed"
+echo "Check src/pages/sandbox/rhf.tsx for example usage"
+
